8f0bcd7c11081ad5db05eef378f25c33a409ab1e
Eric Denovellis <edeno@bu.edu>
2025-11-05 21:44:31 -0500

    Remove custom MathJax startup and options config
    Eliminated custom MathJax startup logic and options from mathjax.js, reverting to default configuration. This simplifies MathJax initialization and removes page-specific typesetting triggers.

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -4,27 +4,5 @@ window.MathJax = {
     displayMath: [["\\[", "\\]"], ['$$', '$$']],
     processEscapes: true,
     processEnvironments: true
-  },
-  options: {
-    ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
-  },
-  startup: {
-    ready: () => {
-      MathJax.startup.defaultReady();
-      MathJax.startup.promise.then(() => {
-        // Typeset any content that was already on the page
-        MathJax.typesetPromise();
-      });
-    }
   }
 };
-
-document$.subscribe(() => {
-  if (typeof MathJax !== 'undefined' && MathJax.typesetPromise) {
-    MathJax.startup.output.clearCache?.();
-    MathJax.typesetClear?.();
-    MathJax.texReset?.();
-    MathJax.typesetPromise();
-  }
-})
